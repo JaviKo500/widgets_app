3ebb79aaeec6394cc5617e8215409957535dff51
JaViKo <javikogutierrez64@gmail.com>
2023-06-24 13:43:53 -0500

    add menu items

---
+++ lib/config/menu/menu_item.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem({
+    required this.title,
+    required this.subTitle,
+    required this.link,
+    required this.icon,
+  });
+}
+
+const appMenuItems = <MenuItem>[
+  MenuItem(title: 'Buttons', subTitle: 'Many buttons in flutter', link: '/buttons', icon: Icons.smart_button_outlined,),
+  MenuItem(title: 'Card', subTitle: 'Container with styles', link: '/card', icon: Icons.credit_card_outlined,),
+];
