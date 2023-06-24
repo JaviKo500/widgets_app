00beba1d9b5bf1c933b5ea2c9e069e9d9d8532dd
JaViKo <javikogutierrez64@gmail.com>
2023-06-24 15:07:13 -0500

    go router config

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_item.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -41,7 +42,8 @@ class _HomeView extends StatelessWidget {
             color: colors.primary,
           ),
           onTap: () {
-            Navigator.pushNamed(context, menuItem.link);
+            // Navigator.pushNamed(context, menuItem.link);
+            context.push(menuItem.link);
           },
         );
       },
