d23ce1a3949e3c5129868014c3f34140a2eef6fa
JaViKo <javikogutierrez64@gmail.com>
2023-07-04 23:00:50 -0500

    menu drawer

--- lib/presentation/widgets/side_menu.dart
+++ lib/presentation/widgets/side_menu.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:widgets_app/config/menu/menu_item.dart';
 
 class SideMenu extends StatefulWidget {
   const SideMenu({Key? key}) : super(key: key);
@@ -9,25 +11,43 @@ class SideMenu extends StatefulWidget {
 
 class _SideMenuState extends State<SideMenu> {
   int navDrawerIndex = 0;
-
   @override
   Widget build(BuildContext context) {
+    final hasNotch = MediaQuery.of(context).viewPadding.top > 35;
     return NavigationDrawer(
         selectedIndex: navDrawerIndex,
         onDestinationSelected: (value) {
           setState(() {
             navDrawerIndex = value;
           });
+          final menuItem = appMenuItems[value];
+          context.pushNamed( menuItem.name );
         },
-        children: const [
-          NavigationDrawerDestination(
-            icon: Icon(Icons.add),
-            label: Text('HomeScreen', style: TextStyle()),
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(28, hasNotch ? 10 : 20, 16, 10),
+            child: const Text('text', style: TextStyle()),
           ),
-          NavigationDrawerDestination(
-            icon: Icon(Icons.add),
-            label: Text('SideScreen', style: TextStyle()),
-          )
+          ...appMenuItems.sublist(0, 3).map(
+                (menu) => NavigationDrawerDestination(
+                  icon: Icon(menu.icon),
+                  label: Text(menu.title, style: const TextStyle()),
+                ),
+              ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(28, 10, 16, 10),
+            child: Divider(),
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(28, 10, 16, 10),
+            child: Text('More options', style: TextStyle()),
+          ),
+          ...appMenuItems.sublist(3).map(
+                (menu) => NavigationDrawerDestination(
+                  icon: Icon(menu.icon),
+                  label: Text(menu.title, style: const TextStyle()),
+                ),
+              ),
         ]);
   }
 }
