a6cb20abf057b45b4949a70265c46c8d85f1fd69
JaViKo <javikogutierrez64@gmail.com>
2023-07-09 13:18:11 -0500

    counter add

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,29 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/presentation/providers/counter_provider.dart';
 
-class CounterScreen extends StatelessWidget {
-  
+class CounterScreen extends ConsumerWidget {
   static const name = 'counter_screen';
 
   const CounterScreen({Key? key}) : super(key: key);
-
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int counterClick = ref.watch( counterProvider );
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Counter Riverpod',
-          style: TextStyle()
-        ),
+        title: const Text('Counter Riverpod', style: TextStyle()),
       ),
       body: Center(
-        child: Text(
-          'Value: 10',
-          style: Theme.of(context).textTheme.titleLarge
-        ),
+        child:
+            Text('Value: $counterClick', style: Theme.of(context).textTheme.titleLarge),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          
+          ref.read( counterProvider.notifier ).state++;
         },
         child: const Icon(Icons.add),
       ),
